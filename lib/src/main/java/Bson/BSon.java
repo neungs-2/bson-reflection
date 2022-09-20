cdfd96e746c99d9fa97b965e9cc2d7879d97e124
DESKTOP-5IDPRVD\user <gnlee95@gmail.com>
2022-09-21 00:32:41 +0900

    Feature: Create getJsonPath method

--- lib/src/main/java/Bson/BSon.java
+++ lib/src/main/java/Bson/BSon.java
@@ -1,8 +1,20 @@
 package Bson;
 
 import org.json.simple.JSONObject;
+import java.util.HashMap;
+
 
 public class BSon {
+	public String getJsonPath(String key) {
+		HashMap<String, String> jsonPath = new HashMap<String, String>();
+		jsonPath.put("BITCOIN", "./lib/src/main/resources/Bitcoin.json");
+		jsonPath.put("ETHEREUM", "./lib/src/main/resources/Ethereum.json");
+		jsonPath.put("USER", "./lib/src/main/resources/User.json");
+
+		return jsonPath.get(key);
+	}
+
+
 	// parse 메서드를 model을 넣어서 실행 --> model에 값이 들어가야 함
 	// model.getType을 이용해 어떤 모델인지 판단
 	public Object parse(Class<?> cls) throws Exception {
