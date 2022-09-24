926d49bbb09506b3d53fccc89f9dfc40bbdae55b
logan <gnlee95@gmail.com>
2022-09-24 23:57:49 +0900

    Feat: Add getType method in User model

--- lib/src/main/java/model/User.java
+++ lib/src/main/java/model/User.java
@@ -4,4 +4,11 @@ public class User {
     public String name;
     public int age;
     public String addr;
+
+    enum Type {
+        USER
+    }
+    public Type getType() {
+        return Type.USER;
+    }
 }
