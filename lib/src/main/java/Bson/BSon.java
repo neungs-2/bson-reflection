dbe72520ce71b4072e1138448e098c8621487964
logan <gnlee95@gmail.com>
2022-09-26 01:23:39 +0900

    Temporary Commit

--- lib/src/main/java/Bson/BSon.java
+++ lib/src/main/java/Bson/BSon.java
@@ -1,5 +1,7 @@
 package Bson;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -32,23 +34,45 @@ public class BSon {
 	// model.getType을 이용해 어떤 모델인지 판단
 	public Object parse(Class<?> cls) throws Exception {
 		// model을 cls로 주입하여 인스턴스 생성하기
+		String jsonPath;
+		String json = "";
+
 
 		Object obj = cls.getDeclaredConstructor().newInstance();
 		Method[] methodArr = obj.getClass().getDeclaredMethods();
-		System.out.println(getJsonPath("USER"));
-		System.out.println(methodArr[0].invoke(obj));
+//		Field[] fieldArr = obj.getClass().getDeclaredField();
+//		System.out.println(getJsonPath("USER"));
+//		System.out.println(methodArr[0].invoke(obj));
 
 		// TODO
 		// obj의 타입을 통해 JSON 가져오기
-		String jsonPath = this.getJsonPath(methodArr[0].invoke(obj).toString());
-		String json = this.getJsonFile(jsonPath);
+		try{
+			for(int i = 0; i < methodArr.length; i++) {
+				if (methodArr[i].getName().equals("getType")) {
+					jsonPath = this.getJsonPath(methodArr[0].invoke(obj).toString());
+					json = this.getJsonFile(jsonPath);
+				}
+			}
+
+			// TODO: 직접 구현하고 싶다면 field에 JSON 값 직접 할당하는 코드 작성
+//			for(int i = 0; i < fieldArr.length; i++) {
+//				if ()
+//			}
+
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		ObjectMapper mapper = new ObjectMapper();
+		Object result = mapper.readValue(json, cls);
+
 
 //		JSONObject jsonObj = new JSONObject();
 
 //		JSONParser jsonParser = new JSONParser();
 //		JSONObject jsonObject = (JSONObject) jsonParser.parse(String.valueOf(cls));
 //		System.out.println(jsonObject);
-		return json;
+		return result;
 	}
 
 	// toJSON에 JSON(resource)를 넣어서 실행 --> Object로 변환
--- lib/src/main/java/app/Application.java
+++ lib/src/main/java/app/Application.java
@@ -3,9 +3,6 @@ package app;
 import Bson.BSon;
 import model.Bitcoin;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 public class Application {
 	private static final String filePath = "./lib/src/main/resources/Bitcoin.json";
 
@@ -13,23 +10,10 @@ public class Application {
 		System.out.println("\n\n@@@@@@@@@@@@@@@@@ Main Method Now Start. @@@@@@@@@@@@@@@@@@\n");
 
 		try {
-			String json = null;
-
-			try {
-				json = new String(Files.readAllBytes(Paths.get(filePath)));
-			} catch (Exception except) {
-				except.printStackTrace();
-			}
-
 //			Class<?> bson = Class.forName("Bson.BSon");
 //			Method[] bsonMethod = bson.getClass().getDeclaredMethods();
 
 			Bitcoin bitcoin = new Bitcoin();
-
-			bitcoin.from = "alice";
-			bitcoin.to = "bob";
-			bitcoin.addr = "addr bitcoin blur blur";
-
 			BSon bson = new BSon();
 
 			System.out.println(bitcoin.getType());
