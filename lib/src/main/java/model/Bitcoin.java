086d65675c31ba3d2bf867df031ccb2ebe583a25
logan <gnlee95@gmail.com>
2022-11-07 15:26:41 +0900

    Feat: 모델에 필드 생성

--- lib/src/main/java/model/Bitcoin.java
+++ lib/src/main/java/model/Bitcoin.java
@@ -1,10 +1,13 @@
 package model;
 
 public class Bitcoin extends Coin {
+    public String addr;
+    public String from;
+    public String to;
+
     @Override
     public Type getType() {
         // TODO Auto-generated method stub
         return Type.BITCOIN;
     }
-
 }
--- lib/src/main/java/model/Ethereum.java
+++ lib/src/main/java/model/Ethereum.java
@@ -1,6 +1,10 @@
 package model;
 
 public class Ethereum extends Coin {
+    public String addr;
+    public String from;
+    public String to;
+
    @Override
    public Type getType() {
        // TODO Auto-generated method stub
